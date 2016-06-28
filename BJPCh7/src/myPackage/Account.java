39bc9568dfd3ed61e3434a0fcf180ec3f63b8adf
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-28 19:57:45 +0100

    Modified

--- BJPCh7/src/myPackage/Account.java
+++ BJPCh7/src/myPackage/Account.java
@@ -4,13 +4,55 @@ import java.math.BigDecimal;
 
 public class Account {
 	// Class variables
-	String name;
-	BigDecimal amount;
+	private String name;
+	private BigDecimal amount;
 	// Constructor
 	public Account(String accountName, String startAmount) {
-		this.name = accountName;
-		this.amount = new BigDecimal(startAmount);
+		setName(accountName);
+		setAmount(startAmount);
 		// Allow for two decimals
 		this.amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+	}
+	// Method
+	public String getName(){
+		return this.name;
+	}
+	// Method
+	public BigDecimal getAmount(){
+		return this.amount;
+	}
+	// Method
+	public void setName(String newName){
+		// Only letters and numbers without any symbols 
+		String pattern = "^[a-zA-Z0-9]*$";
+		if(newName.matches(pattern)){
+			this.name = newName;
+		}
+	}
+	// Method
+	public void setAmount(String newAmount){
+		this.amount = new BigDecimal(newAmount);
+	}
+	// Method
+	public void withdraw(String withdrawal) throws IllegalArgumentException {
+		BigDecimal desiredAmount = new BigDecimal(withdrawal);
+		// If desired amount is negative, throw an exception
+		if(desiredAmount.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException();
+		}
+		// If the amount is less than the desired amount, throw an exception
+		if(amount.compareTo(desiredAmount) < 0) {
+			throw new IllegalArgumentException();	
+		}
+		this.amount = this.amount.subtract(desiredAmount);
+	}
+	// Method
+	public void deposit(String deposit) throws IllegalArgumentException {
+		BigDecimal desiredAmount = new BigDecimal(deposit);
+		// If desired amount is negative, throw an exception 
+		if(desiredAmount.compareTo(BigDecimal.ZERO) < 0){
+			throw new IllegalArgumentException();			
+		}
+		this.amount = this.amount.add(desiredAmount);
 	}
 }
--- BJPCh7/src/myPackage/AccountManager.java
+++ BJPCh7/src/myPackage/AccountManager.java
@@ -3,14 +3,18 @@ package myPackage;
 import java.math.BigDecimal;
 
 public class AccountManager {
-
+	// Method main
 	public static void main(String[] args) {
 		// Variables
 		Account account = new Account("FirstAccount", "10.00");
-		System.out.println("Account Created: " + account.name);
-		System.out.println("Balance: " + account.amount);
-		account.amount = new BigDecimal("-15.00");
-		System.out.println("New Balance: " + account.amount);
+		System.out.println("Account Created: " + account.getName());
+		System.out.println("Balance: " + account.getAmount());
+		try{
+			account.withdraw("20.00");
+		}catch(IllegalArgumentException iae){
+			System.out.println("Invalid Withdrawal");
+		}finally{
+			System.out.println("New Balance: " + account.getAmount());
+		}
 	}
-
 }
