577c9fbf74cd5ab11e8a56b0df458533e55f07d7
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-28 15:02:46 +0100

    Modified

---
+++ BJPCh6/src/myPackage/DistanceConverter.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package myPackage;
+
+/**
+ * @author user
+ *
+ */
+public class DistanceConverter {
+	// Method
+	public static double convertFeetToMeters(double feet){
+		return feet * 0.3048;
+	}
+	// Method
+	public static double convertMetersToFeet(double meters) {
+		return meters * 3.2808;
+	}
+	// Method
+	public static double convertFeetToInches(double feet) {
+		return feet * 12;
+	}
+	// Method
+	public static double convertInchesToFeet(double inches) {
+		return inches / 12;
+	}
+	// Method
+	public static double convertCentimetersToFeet(double centimeters) {
+		return convertMetersToFeet(centimeters / 100);
+	}
+	// Method
+	public static double convertFeetToCentimeters(double feet) {
+		return convertFeetToMeters(feet) * 100;
+	}
+	// Method
+	public static double convertCentimersToInches(double centimeters) {
+		return convertFeetToInches(convertCentimetersToFeet(centimeters));
+	}
+	// Method
+	public static double convertInchesToCentimeters(double inches) {
+		return convertFeetToCentimeters(convertInchesToFeet(inches));
+	}
+}
