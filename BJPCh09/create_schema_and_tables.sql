ca0e1ef27eeaf238b4a9828cd272d2935720bf21
wvanheemstra <willem@vanheemstrasystems.com>
2016-07-08 21:11:41 +0100

    Modified

--- BJPCh09/create_schema_and_tables.sql
+++ BJPCh09/create_schema_and_tables.sql
@@ -19,3 +19,26 @@ CREATE TABLE `employeeschema`.`employee` (
         REFERENCES `employeeschema`.`department` (`DepartmentID`)
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
+
+CREATE TABLE `employeeschema`.`project` (
+  `ProjectID` INT NOT NULL,
+  `ProjectName` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`ProjectID`)
+);
+
+CREATE TABLE `employeeschema`.`works_on` (
+  `EmployeeID` INT(11) NOT NULL,
+  `ProjectID` INT(11) NOT NULL,
+  PRIMARY KEY (`EmployeeID`, `ProjectID`),
+  INDEX `works_onProj_idx` (`ProjectID` ASC),
+  CONSTRAINT `works_onEmp`
+    FOREIGN KEY (`EmployeeID`)
+    REFERENCES `employeeschema`.`employee` (`EmployeeID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `works_onProj`
+    FOREIGN KEY (`ProjectID`)
+    REFERENCES `employeeschema`.`project` (`ProjectID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
